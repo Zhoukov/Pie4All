0b93ca5d3809fbf47e525984878c60da5836fabf
Zhoukov <tinusie_93@hotmail.com>
2014-04-16 12:10:21 +0200

    Still not working

--- Pie4AllApp/src/com/example/pie4allapp/MainActivity.java
+++ Pie4AllApp/src/com/example/pie4allapp/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.pie4allapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.TabActivity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener {
 
+	
+Spinner spinner;
+TextView categorie;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +25,29 @@ public class MainActivity extends Activity {
 		
 		Spinner spinner = (Spinner) findViewById(R.id.categories);
 			String [] categories = PieData.getArray(); 
+			categorie = (TextView) findViewById(R.id.textView1);
+			
 			ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
 					android.R.layout.simple_spinner_item, categories);
 			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			spinner.setAdapter(dataAdapter);
-		
+			spinner.setOnItemSelectedListener(this);
 	}
+		
+			 
+			 public void onItemSelected(AdapterView<?> parent, View view, int position,
+					   long id) {
+				 	  spinner.setSelection(position);
+					  String soort = (String) spinner.getSelectedItem();
+					  categorie.setText(soort);
+					 }
 
-    
+					 @Override
+					 public void onNothingSelected(AdapterView<?> arg0) {
+					  // TODO Auto-generated method stub
+
+					  }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
