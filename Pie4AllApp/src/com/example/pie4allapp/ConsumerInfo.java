3aae7d7c8021ae0f513f92ac2a940242fbcbce96
Zhoukov <tinusie_93@hotmail.com>
2014-04-17 10:40:25 +0200

    Initial push of the day

---
+++ Pie4AllApp/src/com/example/pie4allapp/ConsumerInfo.java
@@ -0,0 +1,5 @@
+package com.example.pie4allapp;
+
+public class ConsumerInfo {
+
+}
---
+++ Pie4AllApp/src/com/example/pie4allapp/DetailView.java
@@ -0,0 +1,93 @@
+package com.example.pie4allapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DetailView extends Activity implements OnClickListener{
+
+String selectedProduct;
+TextView titel;
+TextView detail;
+Button bestellen;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+		
+		Intent myIntent = getIntent();
+		selectedProduct = myIntent.getStringExtra("selectedProduct");
+		
+		System.out.println("Detailview: Selected product: "+selectedProduct);
+		
+		titel = (TextView) findViewById(R.id.titel);
+		titel.setText(selectedProduct);
+		
+		detail = (TextView) findViewById(R.id.detail);
+		detail.setText(PieData.getInstance().getProductInfo(selectedProduct));
+		
+		bestellen = (Button) findViewById(R.id.bestellen);
+			
+	}
+	
+
+
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) 
+        {
+            onBackPressed();
+            System.out.println("Back pressed");
+        }
+       
+		return true;
+	}
+	@Override
+    public void onBackPressed()
+	{
+		Intent i = new Intent(DetailView.this, MainActivity.class);
+		startActivity(i);
+		finish();
+       
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	        	
+	        	//System.out.println("UP Pressed");
+	        	
+	        	Intent i = new Intent(DetailView.this, MainActivity.class);
+	        	startActivity(i);
+	        	
+	        	finish();
+	        	
+	            return(true);
+	    }
+
+	    return(super.onOptionsItemSelected(item));
+	}
+
+
+
+	@Override
+	public void onClick(View v) {
+		
+		
+		
+	}
+
+}
+	
+	
+	
+
